21e642348d0216bdf31cc7de654a1704819a0a36
DESKTOP-9TQ6ND6\guilh <guilherme@epicquestti.com.br>
2021-08-30 15:34:54 -0300

    ajdbhjb

---
+++ src/pages/recuperation.tsx
@@ -0,0 +1,63 @@
+// eslint-disable-next-line no-use-before-define
+import React, { FC, ReactNode } from "react"
+import { Container } from "../styles/pages/Home"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
+import CloseIcon from "@material-ui/icons/Close"
+
+import {
+  Fields2,
+  LoginFrame,
+  SigninButton,
+  Title,
+  Name,
+  ViewLoginFrame,
+  Close
+} from "../styles/pages/recuperation"
+
+interface RegistrationProps {
+  children?: ReactNode
+}
+
+const Registration: FC<RegistrationProps> = () => {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        "& > *": {
+          margin: theme.spacing(1),
+          width: "25ch"
+        }
+      }
+    })
+  )
+
+  return (
+    <>
+      <Container>
+        <ViewLoginFrame>
+          <LoginFrame>
+            <Close>
+              <a href="/authentication">
+                <CloseIcon color="action" />
+              </a>
+            </Close>
+            <Title>Encontre sua conta</Title>{" "}
+            <h3>Insira seu email para procurar a sua conta.</h3>
+            <Fields2>
+              <Name>
+                <input
+                  type="usename"
+                  placeholder="E-mail"
+                  // value={user}
+                  // onChange={setUserContent}
+                />
+              </Name>
+            </Fields2>
+            <SigninButton>Pesquisar</SigninButton>
+          </LoginFrame>
+        </ViewLoginFrame>
+      </Container>
+    </>
+  )
+}
+
+export default Registration
